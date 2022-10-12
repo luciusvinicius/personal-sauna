e12552b6f2cc44acc8c83c3350d85ff21cbc1aaa
Afonso Campos <afonso.campos@ua.pt>
2022-10-12 17:57:33 +0100

    hotfix

--- website/personal-sauna/src/App.js
+++ website/personal-sauna/src/App.js
@@ -4,14 +4,8 @@ import ShowData from "./pages/ShowData";
 import "./css/index.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { algorithm } from './algo/algotithm';
-import { get_day_temp } from './api/API';
-
 
 function App() {
-  // console.log(get_day_temp(12))
-  algorithm(12, 18)
-
   return (
       <>
         <h1 className={"title"}>Personal Sauna</h1>
